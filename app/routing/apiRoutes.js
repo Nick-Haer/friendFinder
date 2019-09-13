481bec42ecc9740f7c0a6b1f1c8a2c3eb61d8ed2
Nick.Haer <nhaer123@gmail.com>
2019-09-12 23:11:55 -0500

    added logic to return the current users closest match, and then to display their information in a modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,23 +19,76 @@ router.get(`/`, function (req, res) {
 
 
 router.post(`/`, function (req, res) {
+
+
     // console.log(req.body)
+
+    // console.log(people)
+
+    let currentPerson = req.body;
+
+    
+
+
+    // console.log(people)
+
+
+
+    function makeMeAMatch (peopleArr) {
+        let matchMakerArray = [];
+        for (let person of peopleArr) {
+            // console.log(person)
+        let counter = -1;
+           let scoresDiff = (person.preferences.reduce((acc, element) => {
+                counter++;
+                return parseInt(acc) + Math.abs(parseInt(element) - parseInt(currentPerson.preferences[counter]))
+            }, 0))
+            // console.log(scoresDiff)
+        let newPerson = {}
+        newPerson.name = person.name
+        newPerson.photo = person.photo
+        newPerson.difference = scoresDiff
+
+        matchMakerArray.push(newPerson)
+        }
+
+        let result = 100;
+
+        for (let match of matchMakerArray) {
+            if (match.difference < result) {
+                result = match.difference
+            }
+        }
+
+        // console.log(result)
+        console.log(matchMakerArray)
+
+        for (let match of matchMakerArray) {
+            if (match.difference === result) {
+                return res.json(match)
+            }
+        }
+
+
+    }
+
+    if (people.length >= 1) {
+        makeMeAMatch(people)
+    }
+
+
+
+
+
+    people.push(currentPerson)
 
     // console.log(people)
 
     
 
-    let matchMakerArray = people.forEach()
-
-    people.push(req.body)
-
-    
-
-    console.log(people)
-
     // console.log(friendsList)
 
-    res.json(req.body)
+    // res.json(req.body)
 
 })
 
